6de849246a70b5fcd4fbf7fc7f42ea45ac70d0df
liuxiang7 <liuxiang7@tal.com>
2022-03-22 08:52:08 +0800

    feat:bind,curry,deepClone

--- js/手写代码/bind.js
+++ js/手写代码/bind.js
@@ -1,3 +1,4 @@
+// 参考网址
 var obj = {
     value: "vortesnail",
     num:1
@@ -8,19 +9,68 @@ function fn(num) {
     console.log(this.num+num);
 }
 
-let bindFn = fn.bind(obj, 2); // vortesnail
-bindFn()
+// let bindFn = fn.bind(obj, 2); // vortesnail
+// bindFn()
 
-Function.prototype.myBind = function (context) { 
+// 1.0 返回一个函数	
+Function.prototype.bind1 = function (context) {
     var self = this;
-    var args = [...arguments].slice(1)
-    var f = function () {
-        // 真正执行的是self，也就是外面的this
-        // 这里面的this是window
-        return self.apply(context || window, args)
+    return function () {
+        return self.apply(context);
     }
-    return f
+
 }
 
-bindFn2 = fn.myBind(obj, 2); // vortesnail
-bindFn2()
+
+// 2.0 传参的模拟实现
+// 我在 bind 的时候，是否可以传参呢？
+// 我在执行 bind 返回的函数的时候，可不可以传参呢？
+
+// 注意：这里写箭头函数会出错
+Function.prototype.bind2 = function(context, ...args) {
+    return (...args2) => { 
+        return this.apply(context, args.concat(args2))
+    }
+}
+
+function test2 () { 
+    var foo = {
+        value: 1
+    };
+
+    function bar(name, age) {
+        console.log(this.value);
+        console.log(name);
+        console.log(age);
+
+    }
+
+    var bindFoo = bar.bind2(foo, 'daisy');
+    bindFoo('18');
+    // 1
+    // daisy
+    // 18
+
+}
+
+test2()
+
+// 3.0 一个绑定函数也能使用new操作符创建对象：
+// 这种行为就像把原函数当成构造器。
+// 提供的 this 值被忽略，同时调用时的参数被提供给模拟函数。
+
+// 也就是说当 bind 返回的函数作为构造函数的时候，bind 时指定的 this 值会失效，但传入的参数依然生效。
+
+// Function.prototype.myBind = function (context) { 
+//     var self = this;
+//     var args = [...arguments].slice(1)
+//     var f = function () {
+//         // 真正执行的是self，也就是外面的this
+//         // 这里面的this是window
+//         return self.apply(context || window, args)
+//     }
+//     return f
+// }
+
+// bindFn2 = fn.myBind(obj, 2); // vortesnail
+// bindFn2()
--- js/手写代码/curry.js
+++ js/手写代码/curry.js
@@ -4,10 +4,11 @@ function add(a, b, c) {
 }
 // curry 编程思想 函数拆分的过程 （组合compose)
 // 函数参数降维处理
-const curry = (fn, args = []) => {
+const curry = (fn, ...args) => {
     // fn.length fn形参的个数
     const length = fn.length;
     return (...arg) => {
+        console.log('arg', arg)
         const _args = [...args, ...arg];
         if (_args.length >= length) {
             return fn(..._args);
@@ -16,7 +17,7 @@ const curry = (fn, args = []) => {
         }
     }
 }
-var _curry = function (fn, args) {
+const curry2 = function (fn, args) {
     var length = fn.length;
         args = args || [];
     return function () {
@@ -31,12 +32,12 @@ var _curry = function (fn, args) {
     }
 }
 
-add(1,2,3)
-const _add = curry(add)
-_add(1)(2)(3)
-_add(1,2)(3)
-_add(1)(2,3)
-_add(1,2,3)
+// add(1,2,3)
+// let _add = curry(add)
+// _add(1)(2)(3)
+// _add(1,2)(3)
+// _add(1)(2,3)
+// _add(1,2,3)
 
 // function isType(type, val) {
 //     return Object.prototype.toString.call(val) === `[object ${type}]`
@@ -45,3 +46,22 @@ _add(1,2,3)
 // const _isType = curry(isType);
 // const isFunction = _isType('Function');
 // const isArray = _isType('Array');
+
+const curry3 =  (fn, ...args) => 
+    // 函数的参数个数可以直接通过函数数的.length属性来访问
+    args.length >= fn.length // 这个判断很关键！！！
+    // 传入的参数大于等于原始函数fn的参数个数，则直接执行该函数
+    ? fn(...args)
+    /**
+     * 传入的参数小于原始函数fn的参数个数时
+     * 则继续对当前函数进行柯里化，返回一个接受所有参数（当前参数和剩余参数） 的函数
+    */
+        : (..._args) => curry3(fn, ...args, ..._args);
+
+add(1,2,3)
+let _add = curry3(add)
+_add(1)(2)(3)
+_add(1,2)(3)
+_add(1)(2,3)
+_add(1,2,3)
+
