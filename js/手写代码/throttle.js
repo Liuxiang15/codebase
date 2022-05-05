59c61dcba712ec3b0e380f2feb125dad5e89459c
liuxiang7 <liuxiang7@tal.com>
2022-05-05 15:21:11 +0800

    节流-先执行

--- js/手写代码/throttle.js
+++ js/手写代码/throttle.js
@@ -3,8 +3,8 @@ function throttle(fn, wait) {
     return function(...args) {
         // 如果没有开启定时器，开启一个
         if (!timer) {
+            fn.apply(this,args)
             timer = setTimeout(function () {
-                fn.apply(this,args)
                 // 执行完fn后将定时器timer清空
                 timer = null;
             }, wait); 
