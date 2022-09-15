23a18c4ecc1fd092b45ae023df6a23b7b6fcf369
liuxiang7 <liuxiang7@tal.com>
2022-09-15 22:52:03 +0800

    axios

--- axios/myAxios.js
+++ axios/myAxios.js
@@ -14,18 +14,20 @@
 
 
 
-class Axios { 
-    constructor() { 
+class Axios {
+    constructor() {
 
     }
 
-    request (config) { 
-        return new Promise(resolve => { 
-            const { url = '', method = 'get', data = {} } = config
+    request(config) {
+        return new Promise(resolve => {
+            const {
+                url = '', method = 'get', data = {}
+            } = config
             // 发送ajax请求
             const xhr = new XMLHttpRequest()
             xhr.open(method, url, true)
-            xhr.onload = function () { 
+            xhr.onload = function () {
                 console.log(xhr.responseText)
                 resolve(xhr.responseText)
             }
@@ -33,11 +35,59 @@ class Axios {
         })
     }
 }
+// 定义get,post...方法，挂在到Axios原型上
+// axios.request(config)
+// axios.get(url[, config])
+// axios.delete(url[, config])
+// axios.head(url[, config])
+// axios.options(url[, config])
+// axios.post(url[, data[, config]])
+// axios.put(url[, data[, config]])
+// axios.patch(url[, data[, config]])
+const methodsArr = ['get', 'delete', 'head', 'options', 'put', 'patch', 'post']
 
-function CreateAxiosFn () { 
+methodsArr.forEach(method => {
+    Axios.prototype[method] = function (url, data = {}, config = {}) {
+        console.log(`执行了${method}方法`)
+        // 2个参数(url[, config])
+        if (['get', 'delete', 'head', 'options'].includes(method)) {
+            // TODO:这里为什么不写data？
+            return this.request({
+                method,
+                url,
+                // data
+                ...arguments[1] || {}
+            })
+        } else {
+            // 3个参数(url[,data[,config]])
+            return this.request({
+                method,
+                url,
+                data,
+                ...arguments[2] || {}
+            })
+        }
+    }
+
+})
+
+const utils = {
+    extend (target, origin, context) { 
+        for (let key in origin) { 
+            if (origin.hasOwnProperty(key)) { 
+                target[key] = origin[key].bind(context)
+            } else {
+                target[key] = origin[key]
+            }
+        }
+    }
+}
+
+function CreateAxiosFn() {
     let axios = new Axios()
     // 问题：为啥导出的是axios的request方法，而不是单独写一个？
     let req = axios.request.bind(axios)
+    utils.extend(req, Axios.prototype, axios)
     return req
 }
 
