45de7755c168bb67f2f6d243bd80c88d3a74433c
liuxiang7 <liuxiang7@100tal.com>
2023-02-14 17:09:33 +0800

    test

--- vue/mini/js/watcher.js
+++ vue/mini/js/watcher.js
@@ -5,31 +5,39 @@ class Watcher {
     /**
      * 初始化
      * @param {*} vm Vue实例
-     * @param {*} key data中的属性
+     * @param {*} expOrFn data中的属性
      * @param {*} cb 回调函数，更新视图的具体方法
      */
-    constructor(vm, key, cb) {
+    constructor(vm, expOrFn, cb) {
         this.vm = vm
-        this.key = key
+        this.expOrFn = expOrFn
         this.cb = cb
         // 把观察者存放在Dep.target中
         Dep.target = this
         // 旧数据 更新视图的时候要进行比较
-        // 还有一点就是 vm[key] 这个时候就触发了 get 方法
+        // 还有一点就是 vm[expOrFn] 这个时候就触发了 get 方法
         // 之前在 get 把 观察者 通过dep.addSub(Dep.target) 添加到了 dep.subs中
-        this.oldValue = vm[key]
+        this.oldValue = vm[expOrFn]
         // Dep.target不用存在了 因为上面已经操作好了
         Dep.target = null
     }
     /**
+     * TODO
+     */
+    get() {}
+    /**
      * 观察者中的必备方法 用来更新视图
      */
-    update () { 
+    update() {
         // 获取新值
-        let newValue = this.vm[this.key]
+        let newValue = this.vm[this.expOrFn]
         // 比较新旧
         if (newValue === this.oldValue) return
         // 调用具体的更新方法
         this.cb(newValue)
     }
 }
+
+/**
+ * 解析简单路径
+ */
