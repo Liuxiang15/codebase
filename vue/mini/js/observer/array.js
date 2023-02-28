59b6e603f4c5c6dbf972304bff4249c7e6af0c9d
liuxiang7 <liuxiang7@100tal.com>
2023-02-28 16:43:02 +0800

    feat：数组拦截器

---
+++ vue/mini/js/observer/array.js
@@ -0,0 +1,15 @@
+const arrayProto = Array.prototype;
+export const arrayMethods = Object.create(arrayProto);
+['push', 'pop', 'shift', 'unshift', 'splice', 'sort', 'reverse'].forEach(method => {
+  // 缓存原始方法
+  const original = arrayProto[method];
+  Object.defineProperty(arrayMethods, method, {
+    value: function mutator(...args) {
+      console.log(`Array的${method}方法被访问`)
+      return original.apply(this, args)
+    },
+    enumerable: false,
+    writable: true,
+    configurable: true
+  })
+})
